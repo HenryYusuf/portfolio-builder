9fd905d44398d053c28e0fe74d12c3a74a7b9797
henryyusuf <baharhenry10@gmail.com>
2025-06-08 22:40:44 +0700

    feat: setup clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,68 @@
-import React from "react";
+"use client"
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import React from "react";
+
+const menuItems = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/Contact",
+  },
+];
 
 function Homepage() {
   return (
-    <div className="p-5 flex flex-col gap-5">
-      <h1 className="text-green-500 font-bold">Homepage</h1>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex justify-between items-center bg-secondary px-20 py-5">
+        <h1 className="font-bold text-3xl text-primary">
+          <b className="text-destructive">P</b>{" "}
+          <span className="text-foreground">|</span> B
+        </h1>
+        <div className="flex justify-end gap-5 items-center">
+          {menuItems.map((item) => (
+            <span
+              key={item.title}
+              className={`p-5 text-sm font-bold text-gray-600 cursor-pointer rounded-md hover:bg-accent ${item.path === '/' ? 'bg-primary text-primary-foreground hover:bg-primary/90' : ''}`}
+            >
+              {item.title}
+            </span>
+          ))}
 
-      <Button className="w-max">Shadcn Button</Button>
+          <Button className="cursor-pointer">Sign In</Button>
+        </div>
+      </div>
 
-      <Input placeholder="Shadcn Input" className="w-max" />
+      <div className="grid grid-cols-2 gap-10 mt-20 h-[70vh] px-20">
+        <div className="flex flex-col justify-center">
+          <div>
+            <h1 className="text-4xl font-bold text-primary">
+              <b className="text-destructive">Portfolio</b>{" "}
+              <span className="text-foreground">|</span> Builder
+            </h1>
+            <p className="text-gray-600 mt-2 text-sm font-semibold">
+              Bangun Cerita Unik Kamu, Tampilkan Bakat dengan Profesional, dan
+              Ciptakan Portofolio Menarik dengan Mudah—Memberdayakan Kreator,
+              Freelancer, dan Inovator untuk Bersinar dan Meraih Peluang Impian.
+            </p>
+          </div>
+        </div>
+        <div>
+          <DotLottieReact
+            src="https://lottie.host/2f68616f-2341-4c6d-8f44-d846f8a13b5d/l98VzVzka8.lottie"
+            loop
+            autoplay
+          />
+        </div>
+      </div>
     </div>
   );
 }
